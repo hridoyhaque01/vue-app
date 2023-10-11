992309a3fdbf9bf32061a910e517e1694e963855
hridoyhaque <hridoyhaque.info>
2023-10-11 18:45:30 +0600

    upload vue project setup

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup>
+const name = 'hridoy'
+</script>
+
+<template>
+  <h1>hello world</h1>
+  <p>Using text interpolation: {{ name }}</p>
+  <p>Using v-html directive: <span v-html="rawHtml"></span></p>
+</template>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+// import './assets/main.css'
+
+import { createApp,VueElement } from 'vue'
+import App from './App.vue'
+
+createApp(App).mount('#app')
